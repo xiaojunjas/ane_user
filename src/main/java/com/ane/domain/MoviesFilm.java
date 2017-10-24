4aba03504eed771427148bd09dae0d16d08f6574
xiaojun <xiaojun@DESKTOP-0UAACC9>
2017-10-24 22:02:06 +0800

    x

--- src/main/java/com/ane/domain/MoviesFilm.java
+++ src/main/java/com/ane/domain/MoviesFilm.java
@@ -2,11 +2,19 @@ package com.ane.domain;
 
 
 public class MoviesFilm {
-	
-	private Long id;
-	private Long filmId;
-	private Long moviesId;
-	private int isDeleted;
+	 
+	 private Long id;
+	 private Long filmId;
+	 private Long moviesId;
+	 private String filmName;
+	 private String director;
+	 private String toStar;
+	 private String releaseTime;
+	 private String score;
+	 private String moviesName;
+	 private String address;
+	 private String longitude;
+	 private String latitude;
 	
 	public Long getId() {
 		return id;
@@ -26,11 +34,59 @@ public class MoviesFilm {
 	public void setMoviesId(Long moviesId) {
 		this.moviesId = moviesId;
 	}
-	public int getIsDeleted() {
-		return isDeleted;
+	public String getFilmName() {
+		return filmName;
 	}
-	public void setIsDeleted(int isDeleted) {
-		this.isDeleted = isDeleted;
+	public void setFilmName(String filmName) {
+		this.filmName = filmName;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	public String getToStar() {
+		return toStar;
+	}
+	public void setToStar(String toStar) {
+		this.toStar = toStar;
+	}
+	public String getReleaseTime() {
+		return releaseTime;
+	}
+	public void setReleaseTime(String releaseTime) {
+		this.releaseTime = releaseTime;
+	}
+	public String getScore() {
+		return score;
+	}
+	public void setScore(String score) {
+		this.score = score;
+	}
+	public String getMoviesName() {
+		return moviesName;
+	}
+	public void setMoviesName(String moviesName) {
+		this.moviesName = moviesName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
 	}
 	 
 }
